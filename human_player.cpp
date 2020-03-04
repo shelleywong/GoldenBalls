b2623afd1a23506d353a1745d359a7f863b2519e
piradeepan <48875832+piradeepan@users.noreply.github.com>
2020-03-04 14:30:42 -0800

    Update human_player.cpp

--- human_player.cpp
+++ human_player.cpp
@@ -1,1 +1,12 @@
-#This file is for Human Player class file
+#include <iostream>
+#include human_player.h
+using namespace std;
+HumanPlayer::HumanPlayer() {
+  pdecision = undefined;
+  pstatus = 0;
+}
+HumanPlayer::HumanPlayer( decision type, unsigned status ) {
+  pdecision = type;
+  status = pstatus;
+}
+unsigned HumanPlayer::get_
