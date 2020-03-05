5993b22cddbc2e0bc784c62bb1d9f56e2648e867
piradeepan <48875832+piradeepan@users.noreply.github.com>
2020-03-04 17:03:33 -0800

    Update human_player.cpp

--- human_player.cpp
+++ human_player.cpp
@@ -6,7 +6,7 @@ Human_Player::Human_Player(string name) {
     _name = undefined;
 }
 void Human_Player::set_name( string name ) const {
-    cout<<"Enter player name: ";
+    cout<<"Enter player name: "<<endl;
     cin>> name;
    _name = name;
 }
