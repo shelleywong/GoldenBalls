41c0e71df192ff6175f9461880eaf79dca95a687
piradeepan <48875832+piradeepan@users.noreply.github.com>
2020-03-04 16:57:11 -0800

    Update human_player.cpp

--- human_player.cpp
+++ human_player.cpp
@@ -6,6 +6,8 @@ Human_Player::Human_Player(string name) {
     _name = undefined;
 }
 string Human_Player::set_name( string name ) const {
+    cout<<"Enter the name";
+    cin>> name;
    _name = name;
 }
 Human_Player::Decision Human_Player::split_or_steal( Decision deal ) const {
