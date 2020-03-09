c4847bb1352aa979fa12b2cc0cb841b606c6bfbc
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-03-09 13:47:43 -0700

    Fix Player interface pure functions

--- Player.h
+++ Player.h
@@ -3,14 +3,12 @@
 #include "Decision.h"
 #include <string>
 
-const int INTERFACE_FUNCTION = 0;
-
 class Player
 {
     public:
-        virtual void set_name() = INTERFACE_FUNCTION;
-        virtual Decision split_or_steal() = INTERFACE_FUNCTION;
-        virtual string get_name() = INTERFACE_FUNCTION;        
+        virtual void set_name() = 0;
+        virtual Decision split_or_steal() = 0;
+        virtual string get_name() = 0;
 };
 
 #endif
