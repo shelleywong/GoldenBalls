1f9193273836c1523d285d8d5fdb0f8134ae5698
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-03-09 13:19:17 -0700

    Add Player interface

---
+++ Player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "Decision.h"
+#include <string>
+
+const int INTERFACE_FUNCTION = 0;
+
+class Player
+{
+    public:
+        virtual void set_name() = INTERFACE_FUNCTION;
+        virtual Decision split_or_steal() = INTERFACE_FUNCTION;
+        virtual string get_name() = INTERFACE_FUNCTION;        
+};
