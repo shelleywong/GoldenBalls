c3831e52e1056d38f3081c0237bff7ed22950718
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-03-09 13:10:41 -0700

    Refactor code smells from main

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 #include "Records.h"
 #include "Game.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 Game_Type select_game_type()
 {
@@ -20,22 +21,22 @@ Game_Type select_game_type()
     int G_Type;
     cin >> G_Type;
 
-    label1 : switch (G_Type)
-    {
-    case 1:
-        GT = Game_Type::single;
-        return GT;
-        break;
+    do{
+        switch (G_Type)
+        {
+        case 1:
+            GT = Game_Type::single;
+            return GT;
 
-    case 2:
-        GT = Game_Type::dual;
-        return GT;
-        break;
+        case 2:
+            GT = Game_Type::dual;
+            return GT;
 
-    default:
-        goto label1;
-        break;
-    }
+        default:
+            std::cout<<"Please choose 1 or 2\n";
+            break;
+        }
+    }while( G_Type != 1 && G_Type != 2 );
 }
 
 void show_records()
@@ -47,34 +48,37 @@ void show_records()
 void show_menu()
 {
 
-    Game GM;
+    Game* game;
 
     cout << "1. Press 1 to play the game" << endl
-         << "2. Press 2 to see records" << endl;
+         << "2. Press 2 to see records" << endl
+         << "3. Press 3 to quit" << endl;
 
     int G_Menu;
+    bool quit = false;
     cin >> G_Menu;
 
-    label : switch (G_Menu)
-    {
-    case 1:
-        Game_Type GT = select_game_type();
-        GM(GT);
-        show_records();
-        break;
+    do{
+        switch (G_Menu)
+        {
+            case 1:
+                Game_Type GT = select_game_type();
+                game = new Game(GT);
+                break;
 
-    case 2:
-        show_records();
-        break;
-    
-    case 3:
-        exit(0);
+            case 2:
+                show_records();
+                break;
+            
+            case 3:
+                quit = true;
 
-    default:
-        cout<<"You have entered an incorrect entry."<<endl; 
-        goto label;
-        break;
-    }
+            default:
+                cout<<"You have entered an incorrect entry."<<endl; 
+                break;
+        }
+    }while( G_Menu < 1 || G_Menu > 3);
+
 }
 
 int main()
